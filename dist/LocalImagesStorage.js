e12e9821091c9dde6e2792a1b405720d8f52c8cc
Teffen Ellis <teffen@nirri.us>
2021-12-09 00:30:37 -0500

    idd.

--- dist/LocalImagesStorage.js
+++ dist/LocalImagesStorage.js
@@ -1,28 +1,38 @@
 "use strict";
-// # Local File System Image Storage module
-// The (default) module for storing images, using the local file system
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.LocalImagesStorage = void 0;
-const config_1 = require("./config");
-const LocalStorageBase_1 = require("./LocalStorageBase");
-const url_utils_1 = require("./url-utils");
+// # Local File Base Storage module
+// The (default) module for storing files using the local file system
+const serveStatic = require("express").static;
 const fs_1 = require("fs");
-const sqip_1 = require("sqip");
 const path = require("path");
-let messages = {
+const StorageBase = require("ghost-storage-base");
+const url_utils_1 = require("./url-utils");
+const config_1 = require("./config");
+const sqip_1 = require("sqip");
+const moment = require("moment");
+const errors = require("@tryghost/errors");
+const tpl = require("@tryghost/tpl");
+const logging = require("@tryghost/logging");
+const constants = require("@tryghost/constants");
+const messages = {
     notFound: "Image not found",
     notFoundWithRef: "Image not found: {file}",
     cannotRead: "Could not read image: {file}",
+    invalidUrlParameter: `The URL "{url}" is not a valid URL for this site.`,
 };
-class LocalImagesStorage extends LocalStorageBase_1.LocalStorageBase {
-    storage = new LocalImagesStorage();
+class LocalImagesStorage extends StorageBase {
+    storagePath;
+    staticFileURLPrefix;
+    siteUrl;
+    staticFileUrl;
+    errorMessages;
     constructor() {
-        super({
-            storagePath: path.join(config_1.default.paths.contentPath, "images/"),
-            staticFileURLPrefix: url_utils_1.default.STATIC_IMAGE_URL_PREFIX,
-            siteUrl: config_1.default.url,
-            errorMessages: messages,
-        });
+        super();
+        this.storagePath = path.join(config_1.default.paths.contentPath, "images/");
+        this.staticFileURLPrefix = url_utils_1.default.STATIC_IMAGE_URL_PREFIX;
+        this.siteUrl = config_1.default.url;
+        this.errorMessages = messages;
     }
     /**
      * Saves the file to storage (the file system)
@@ -34,7 +44,26 @@ class LocalImagesStorage extends LocalStorageBase_1.LocalStorageBase {
         const sqipResult = Array.isArray(res) ? res[0] : res;
         const targetFilename = (await this.getUniqueFileName(image, targetDir)) + ".sqip.svg";
         await this.saveRaw(sqipResult.content, targetFilename);
-        return super.save(image, targetDir);
+        return this._save(image, targetDir);
+    }
+    /**
+     * Saves the file to storage (the file system)
+     * - returns a promise which ultimately returns the full url to the uploaded file
+     */
+    async _save(file, targetDir) {
+        let targetFilename;
+        // NOTE: the base implementation of `getTargetDir` returns the format this.storagePath/YYYY/MM
+        targetDir = targetDir || this.getTargetDir(this.storagePath);
+        const filename = await this.getUniqueFileName(file, targetDir);
+        targetFilename = filename;
+        await fs_1.promises.mkdir(targetDir);
+        await fs_1.promises.copyFile(file.path, targetFilename);
+        // The src for the image must be in URI format, not a file system path, which in Windows uses \
+        // For local file system storage can use relative path so add a slash
+        const fullUrl = url_utils_1.default
+            .urlJoin("/", url_utils_1.default.getSubdir(), this.staticFileURLPrefix, path.relative(this.storagePath, targetFilename))
+            .replace(new RegExp(`\\${path.sep}`, "g"), "/");
+        return fullUrl;
     }
     /**
      * Saves a buffer in the targetPath
@@ -52,6 +81,116 @@ class LocalImagesStorage extends LocalStorageBase_1.LocalStorageBase {
             .urlJoin("/", url_utils_1.default.getSubdir(), this.staticFileURLPrefix, targetPath)
             .replace(new RegExp(`\\${path.sep}`, "g"), "/");
         return fullUrl;
+    }
+    /**
+     *
+     * @param {String} url full url under which the stored content is served, result of save method
+     * @returns {String} path under which the content is stored
+     */
+    urlToPath(url) {
+        let filePath;
+        if (url.match(this.staticFileUrl)) {
+            filePath = url.replace(this.staticFileUrl, "");
+            filePath = path.join(this.storagePath, filePath);
+        }
+        else {
+            throw new errors.IncorrectUsageError({
+                message: tpl(messages.invalidUrlParameter, { url }),
+            });
+        }
+        return filePath;
+    }
+    exists(fileName, targetDir) {
+        const filePath = path.join(targetDir || this.storagePath, fileName);
+        return fs_1.promises
+            .stat(filePath)
+            .then(() => {
+            return true;
+        })
+            .catch(() => {
+            return false;
+        });
+    }
+    /**
+     * For some reason send divides the max age number by 1000
+     * Fallthrough: false ensures that if an image isn't found, it automatically 404s
+     * Wrap server static errors
+     *
+     * @returns {serveStaticContent}
+     */
+    serve() {
+        const { storagePath, errorMessages } = this;
+        return function serveStaticContent(req, res, next) {
+            const startedAtMoment = moment();
+            return serveStatic(storagePath, {
+                maxAge: constants.ONE_YEAR_MS,
+                fallthrough: false,
+                onEnd: () => {
+                    logging.info("LocalStorageBase.serve", req.path, moment().diff(startedAtMoment, "ms") + "ms");
+                },
+            })(req, res, (err) => {
+                if (err) {
+                    if (err.statusCode === 404) {
+                        return next(new errors.NotFoundError({
+                            message: tpl(errorMessages.notFound),
+                            code: "STATIC_FILE_NOT_FOUND",
+                            property: err.path,
+                        }));
+                    }
+                    if (err.statusCode === 400) {
+                        return next(new errors.BadRequestError({ err: err }));
+                    }
+                    if (err.statusCode === 403) {
+                        return next(new errors.NoPermissionError({ err: err }));
+                    }
+                    return next(new errors.InternalServerError({ err: err }));
+                }
+                next();
+            });
+        };
+    }
+    async delete(fileName, targetDir) {
+        const filePath = path.join(targetDir, fileName);
+        await fs_1.promises.rm(filePath);
+        return true;
+    }
+    /**
+     * Reads bytes from disk for a target file
+     * - path of target file (without content path!)
+     *
+     * @param options
+     */
+    read(options) {
+        options = options || {};
+        // remove trailing slashes
+        options.path = (options.path || "").replace(/\/$|\\$/, "");
+        const targetPath = path.join(this.storagePath, options.path);
+        return new Promise((resolve, reject) => {
+            fs_1.promises.readFile(targetPath)
+                .then(resolve)
+                .catch((err) => {
+                if (err.code === "ENOENT" || err.code === "ENOTDIR") {
+                    return reject(new errors.NotFoundError({
+                        err: err,
+                        message: tpl(this.errorMessages.notFoundWithRef, {
+                            file: options.path,
+                        }),
+                    }));
+                }
+                if (err.code === "ENAMETOOLONG") {
+                    return reject(new errors.BadRequestError({ err: err }));
+                }
+                if (err.code === "EACCES") {
+                    return reject(new errors.NoPermissionError({ err: err }));
+                }
+                return reject(new errors.InternalServerError({
+                    err: err,
+                    message: tpl(this.errorMessages.cannotRead, {
+                        file: options.path,
+                    }),
+                }));
+            });
+        });
     }
 }
 exports.LocalImagesStorage = LocalImagesStorage;
--- src/LocalImagesStorage.ts
+++ src/LocalImagesStorage.ts
@@ -1,34 +1,41 @@
-// # Local File System Image Storage module
-// The (default) module for storing images, using the local file system
+// # Local File Base Storage module
+// The (default) module for storing files using the local file system
+const serveStatic = require("express").static;
 
-import config from "./config";
-import { LocalStorageBase } from "./LocalStorageBase";
-import urlUtils from "./url-utils";
 import { promises as fs } from "fs";
-import { sqip } from "sqip";
-import * as StorageBase from "ghost-storage-base";
-
 import * as path from "path";
+import * as StorageBase from "ghost-storage-base";
+import urlUtils from "./url-utils";
+import config from "./config";
+import { sqip } from "sqip";
 
-let messages = {
+const moment = require("moment");
+const errors = require("@tryghost/errors");
+const tpl = require("@tryghost/tpl");
+const logging = require("@tryghost/logging");
+const constants = require("@tryghost/constants");
+
+const messages = {
   notFound: "Image not found",
   notFoundWithRef: "Image not found: {file}",
   cannotRead: "Could not read image: {file}",
+  invalidUrlParameter: `The URL "{url}" is not a valid URL for this site.`,
 };
 
-export class LocalImagesStorage
-  extends LocalStorageBase
-  implements StorageBase
-{
-  storage = new LocalImagesStorage();
+export class LocalImagesStorage extends StorageBase {
+  storagePath: string;
+  staticFileURLPrefix: string;
+  siteUrl: string;
+  staticFileUrl: string;
+  errorMessages: Record<string, string>;
 
   constructor() {
-    super({
-      storagePath: path.join(config.paths.contentPath, "images/"),
-      staticFileURLPrefix: urlUtils.STATIC_IMAGE_URL_PREFIX,
-      siteUrl: config.url,
-      errorMessages: messages,
-    });
+    super();
+
+    this.storagePath = path.join(config.paths.contentPath, "images/");
+    this.staticFileURLPrefix = urlUtils.STATIC_IMAGE_URL_PREFIX;
+    this.siteUrl = config.url;
+    this.errorMessages = messages;
   }
 
   /**
@@ -48,7 +55,38 @@ export class LocalImagesStorage
 
     await this.saveRaw(sqipResult.content, targetFilename);
 
-    return super.save(image, targetDir);
+    return this._save(image, targetDir);
+  }
+
+  /**
+   * Saves the file to storage (the file system)
+   * - returns a promise which ultimately returns the full url to the uploaded file
+   */
+  async _save(file: StorageBase.Image, targetDir: string): Promise<string> {
+    let targetFilename: string;
+
+    // NOTE: the base implementation of `getTargetDir` returns the format this.storagePath/YYYY/MM
+    targetDir = targetDir || this.getTargetDir(this.storagePath);
+
+    const filename = await this.getUniqueFileName(file, targetDir);
+
+    targetFilename = filename;
+    await fs.mkdir(targetDir);
+
+    await fs.copyFile(file.path, targetFilename);
+
+    // The src for the image must be in URI format, not a file system path, which in Windows uses \
+    // For local file system storage can use relative path so add a slash
+    const fullUrl = urlUtils
+      .urlJoin(
+        "/",
+        urlUtils.getSubdir(),
+        this.staticFileURLPrefix,
+        path.relative(this.storagePath, targetFilename)
+      )
+      .replace(new RegExp(`\\${path.sep}`, "g"), "/");
+
+    return fullUrl;
   }
 
   /**
@@ -70,5 +108,144 @@ export class LocalImagesStorage
       .replace(new RegExp(`\\${path.sep}`, "g"), "/");
 
     return fullUrl;
+  }
+
+  /**
+   *
+   * @param {String} url full url under which the stored content is served, result of save method
+   * @returns {String} path under which the content is stored
+   */
+  urlToPath(url) {
+    let filePath;
+
+    if (url.match(this.staticFileUrl)) {
+      filePath = url.replace(this.staticFileUrl, "");
+      filePath = path.join(this.storagePath, filePath);
+    } else {
+      throw new errors.IncorrectUsageError({
+        message: tpl(messages.invalidUrlParameter, { url }),
+      });
+    }
+
+    return filePath;
+  }
+
+  exists(fileName, targetDir) {
+    const filePath = path.join(targetDir || this.storagePath, fileName);
+
+    return fs
+      .stat(filePath)
+      .then(() => {
+        return true;
+      })
+      .catch(() => {
+        return false;
+      });
+  }
+
+  /**
+   * For some reason send divides the max age number by 1000
+   * Fallthrough: false ensures that if an image isn't found, it automatically 404s
+   * Wrap server static errors
+   *
+   * @returns {serveStaticContent}
+   */
+  serve() {
+    const { storagePath, errorMessages } = this;
+
+    return function serveStaticContent(req, res, next) {
+      const startedAtMoment = moment();
+
+      return serveStatic(storagePath, {
+        maxAge: constants.ONE_YEAR_MS,
+        fallthrough: false,
+        onEnd: () => {
+          logging.info(
+            "LocalStorageBase.serve",
+            req.path,
+            moment().diff(startedAtMoment, "ms") + "ms"
+          );
+        },
+      })(req, res, (err) => {
+        if (err) {
+          if (err.statusCode === 404) {
+            return next(
+              new errors.NotFoundError({
+                message: tpl(errorMessages.notFound),
+                code: "STATIC_FILE_NOT_FOUND",
+                property: err.path,
+              })
+            );
+          }
+
+          if (err.statusCode === 400) {
+            return next(new errors.BadRequestError({ err: err }));
+          }
+
+          if (err.statusCode === 403) {
+            return next(new errors.NoPermissionError({ err: err }));
+          }
+
+          return next(new errors.InternalServerError({ err: err }));
+        }
+
+        next();
+      });
+    };
+  }
+  async delete(fileName: string, targetDir: string): Promise<boolean> {
+    const filePath = path.join(targetDir, fileName);
+    await fs.rm(filePath);
+
+    return true;
+  }
+
+  /**
+   * Reads bytes from disk for a target file
+   * - path of target file (without content path!)
+   *
+   * @param options
+   */
+  read(options) {
+    options = options || {};
+
+    // remove trailing slashes
+    options.path = (options.path || "").replace(/\/$|\\$/, "");
+
+    const targetPath = path.join(this.storagePath, options.path);
+
+    return new Promise<Buffer>((resolve, reject) => {
+      fs.readFile(targetPath)
+        .then(resolve)
+        .catch((err) => {
+          if (err.code === "ENOENT" || err.code === "ENOTDIR") {
+            return reject(
+              new errors.NotFoundError({
+                err: err,
+                message: tpl(this.errorMessages.notFoundWithRef, {
+                  file: options.path,
+                }),
+              })
+            );
+          }
+
+          if (err.code === "ENAMETOOLONG") {
+            return reject(new errors.BadRequestError({ err: err }));
+          }
+
+          if (err.code === "EACCES") {
+            return reject(new errors.NoPermissionError({ err: err }));
+          }
+
+          return reject(
+            new errors.InternalServerError({
+              err: err,
+              message: tpl(this.errorMessages.cannotRead, {
+                file: options.path,
+              }),
+            })
+          );
+        });
+    });
   }
 }
